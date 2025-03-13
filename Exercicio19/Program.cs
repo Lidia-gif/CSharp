ea2da27be6e6c2c8111e160dba7e9243cd9efc09
Lidia-gif <lidiamaria.202069@gmail.com>
2025-03-13 11:57:14 +0000

    Add files via upload

--- Exercicio19/Program.cs
+++ Exercicio19/Program.cs
@@ -6,59 +6,84 @@ public class Exercicio19
 
     public static void Main(string[] args)
     {
+        //Variaveis Globais
         //Inicia o dicionário
         Dictionary<string, string> numero  = new Dictionary<string, string>();
 
+        string opcao;
+        do
+        {
+            //Menu principal
+            Console.WriteLine("1.Adicionar contacto: ");
+            Console.WriteLine("2.Pesquisar contacto: ");
+            Console.WriteLine("3.Remover conttactos: ");
+            Console.WriteLine("4.sair");
 
-        //Meno principal
-        Console.WriteLine("1.Adicionar contacto: ");
-        Console.WriteLine("2.Pesquisar contacto: ");
-        Console.WriteLine("3.Remover conttactos: ");
-        Console.WriteLine("4.sair");
+            // Pedir ao utilizador que escolha uma opção
+            Console.WriteLine("\nEscolha a opção: ");
 
-        // Pedir ao utilizador que escolha uma opção
-        Console.WriteLine("Escolha a opção: ");
-       
 
-        //Obtem a opcao para usar no swich
-        string opcao = Console.ReadLine();
-       
+            //Obtem a opcao para usar no swich
+             opcao = Console.ReadLine();
 
-        switch (opcao)
-        { 
-              case "1" :
-                Console.WriteLine("Nome: ");
-                String nome = Console.ReadLine();
-                Console.WriteLine("Contacto: ");
-                String contacto = Console.ReadLine();
-                Console.WriteLine("Número adicinado com sucesso! ");
 
-                numero.Add(nome, contacto);
-                break;
+            switch (opcao)
+            {
+                case "1":
+                    Console.WriteLine("Nome: ");
+                    String nome = Console.ReadLine();
+                    Console.WriteLine("Contacto: ");
+                    String contacto = Console.ReadLine();
 
-        
-              case "2":
-                Console.WriteLine("Pesquisar contacto: ");
-                String pesquisarcontacto = Console.ReadLine();
-               
-              
-               
-                
+                    numero.Add(nome, contacto);
 
-                if (numero.ContainsKey(pesquisarcontacto))
-                {
-                    Console.WriteLine("Então o contacto esta correto");
-                }
+                    Console.WriteLine("Número adicinado com sucesso! ");
 
-                else
-                {
-                    Console.WriteLine("O contacto esta errado ");
-                }
-                break;
-        }
-        
-            
 
+                    break;
+
+
+                case "2":
+                    Console.WriteLine("Nome: ");
+                    String nomePesquisado = Console.ReadLine();
+
+
+                    if (numero.ContainsKey(nomePesquisado))// verifica se o contacto é true
+                    {
+                        Console.WriteLine($"\n numero: {numero[nomePesquisado]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("O contacto esta errado ");
+                    }
+                    break;
+
+                case "3":
+                    Console.WriteLine("Nome:");
+                    nomePesquisado = Console.ReadLine();
+
+                    if (numero.ContainsKey(nomePesquisado))
+                    {
+                        numero.Remove(nomePesquisado);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Contacto não encontrado");
+                    }
+                    break;
+
+                case "4":
+                    Console.WriteLine("Sair");
+                   
+
+                    break;
+
+                default:
+                    Console.WriteLine("Opcao inválida");
+                    break;
+
+            }
+        } while (opcao != "4");
         
     }
 }
